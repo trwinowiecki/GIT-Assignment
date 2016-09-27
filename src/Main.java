ea820f50ab1cc8c2a9f0e321d42f4489066b14f4
Taylor Winowiecki <trw0511@gmail.com>
2016-09-27 19:31:25 -0400

    Main() now calls Ninety_Nine_Bottles_of_Beer() function

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        Ninety_Nine_Bottles_of_Beer()
     }
 
-    private static void Ninety_Nine_Bottles_of_Beer{
-
+    private static void Ninety_Nine_Bottles_of_Beer() {
+        
     }
 }
 
