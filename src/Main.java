2c33450220bb74b36cf09f6abe5c9c8738e521dc
Taylor Winowiecki <trw0511@gmail.com>
2016-09-27 19:32:13 -0400

    Added missing semicolon

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Ninety_Nine_Bottles_of_Beer()
+        Ninety_Nine_Bottles_of_Beer();
     }
 
     private static void Ninety_Nine_Bottles_of_Beer() {
