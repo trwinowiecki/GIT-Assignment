41c017af80909b1a01566256be5d9c4d6fde679d
Taylor Winowiecki <trw0511@gmail.com>
2016-09-27 19:38:22 -0400

    Added basic lines of song and comment about them

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
     }
 
     private static void Ninety_Nine_Bottles_of_Beer() {
+        //Basic lines of song for conciseness in code
+        String BoBs = "bottles of beer", BoB = "bottle of beer", OtW = "on the wall",
+                ToD = "take one down, pass it around";
+
         for (int i = 99; i >= 0; i--) {
             
         }
